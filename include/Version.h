72c77dcd60926ddafcc54b60b66d5f0feea00141
Raspberry Pi <raspberrypisig@gmail.com>
2021-01-24 07:17:12 +1100

    new photos for stand

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.123"
+        #define VERSION "0.1.124"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-01-23 14:47:04.006520"
+        #define BUILD_TIMESTAMP "2021-01-24 07:14:05.819293"
     #endif
     
